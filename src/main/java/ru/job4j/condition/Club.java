6213f688f9f49c64ec02a0cd650fe685f9164b95
nskdamir <nsk.damir@gmail.com>
2022-10-13 21:59:35 +0500

    4.3.2. Сокращенный оператор || [#333784 #350269]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -1,6 +1,6 @@
 package ru.job4j.condition;
 
-public class Club {
+public class Club  {
     public static void permission(boolean hasMoney, boolean beFriend) {
         if (hasMoney || beFriend) {
             System.out.println("I can go to the club.");

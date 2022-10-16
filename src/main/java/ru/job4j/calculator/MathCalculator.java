09fe108244de7d049b106b62ca02f81b6e947b19
nskdamir <nsk.damir@gmail.com>
2022-10-16 22:33:38 +0500

    1.1. Что такое import [#504773 #349877]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,7 +1,8 @@
 package ru.job4j.calculator;
 
 import static ru.job4j.math.MathFunction.*;
-public class MathCalculator  {
+
+public class MathCalculator {
 
     public static double sumAndMultiply(double first, double second) {
         return sum(first, second)
@@ -9,22 +10,21 @@ public class MathCalculator  {
     }
 
     public static double sumAndminusdivide(double first, double second) {
-        return sum(first, second)
-                + minus(first, second)
+        return
+                +minus(first, second)
                 + divide(first, second);
     }
 
-    public static double sumAndfour(double first, double second) {
+    public static double sumAll(double first, double second) {
         return sum(first, second)
                 + minus(first, second)
                 + divide(first, second)
-                + multiply(first, second)
-                + sum(first, second);
+                + multiply(first, second);
     }
 
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета  суммы двух операций из класса MathFunction: разность двух чисел и деление двух чисел равен: " + sumAndminusdivide(10, 20));
-        System.out.println("Результат расчета  суммы вычисления всех четырех операций из класса MathFunction равен: " + sumAndfour(10, 20));
+        System.out.println("Результат расчета  суммы вычисления всех четырех операций из класса MathFunction равен: " + sumAll(10, 20));
     }
 }

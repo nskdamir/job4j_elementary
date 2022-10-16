1bd8c3fd8baa70e931739a894c6d3f84f29e1ce4
nskdamir <nsk.damir@gmail.com>
2022-10-16 15:21:22 +0500

    6.0.1. Объявление массива. [#3613]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,21 +1,8 @@
 package ru.job4j.array;
-
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        System.out.println("Размер массива равен: " + ages.length);
         String[] surnames = new String[100500];
-        System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + prices.length);
-        String[] names = new String[4];
-        names[0] = "Damir";
-        names[1] = "Vika";
-        names[2] = "Lenar";
-        names[3] = "Makaresh";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
     }
 }
